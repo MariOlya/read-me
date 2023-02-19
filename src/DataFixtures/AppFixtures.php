53e297e8db376385fe1112f6ba458eb7a18c7f8a
Olga Marinina <youarewonderful@yandex.ru>
2023-02-19 13:27:51 +0700

    Add strict fixtures for tables

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\FileType;
+use App\Entity\PostType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,56 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadFileType($manager);
+        $this->loadPostType($manager);
+    }
+
+    public function loadFileType(ObjectManager $manager): void
+    {
+        $types = ['avatar', 'post', 'ad', 'message'];
+
+        foreach ($types as $type) {
+            $fileType = new FileType();
+            $fileType->setType($type);
+
+            $manager->persist($fileType);
+        }
+
+        $manager->flush();
+    }
+
+    public function loadPostType(ObjectManager $manager): void
+    {
+        $types = [
+            [
+                'type' => 'photo',
+                'icon' => '#icon-filter-photo'
+            ],
+            [
+                'type' => 'video',
+                'icon' => '#icon-filter-video'
+            ],
+            [
+                'type' => 'text',
+                'icon' => '#icon-filter-text'
+            ],
+            [
+                'type' => 'quote',
+                'icon' => '#icon-filter-quote'
+            ],
+            [
+                'type' => 'link',
+                'icon' => '#icon-filter-link'
+            ]
+        ];
+
+        foreach ($types as $type) {
+            $postType = new PostType();
+            $postType->setType($type['type']);
+            $postType->setIcon($type['icon']);
+
+            $manager->persist($postType);
+        }
 
         $manager->flush();
     }
